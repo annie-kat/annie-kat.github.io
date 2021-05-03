046ed262fe7999d6372c57b26a1e885cc09147c3
Annie <annekalford@gmail.com>
2021-05-03 15:28:34 -0500

    Make cell show X when clicked

--- react-gobble/src/components/Square.js
+++ react-gobble/src/components/Square.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function Square(props) {
+  const [value, setValue] = useState();
   return (
-    <button className="square" onClick={props.onClick}>
-      {props.value}
+    <button className="square" onClick={() => setValue('X') }>
+      {value}
     </button>
   );
 }
